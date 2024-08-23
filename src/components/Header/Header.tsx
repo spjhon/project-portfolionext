a5e92447df73f4eb9b311956cbbefe5e29da9c1e
spjhon <spjhon@gmail.com>
2024-08-22 21:40:33 -0500

    se instala next themes y se adiciona dark y light

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+// components/Header.tsx
+
+import Link from "next/link";
+
+
+export default function Header() {
+    return (
+        <header className="bg-gray-200 shadow">
+            <nav className="container flex px-2 py-2 gap-5 ">
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+            </nav>
+        </header>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,15 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: '#8ecae6',
+        secondary: '#219ebc',
+        accent: '#023047',
+        highlight: '#ffb703',
+        warning: '#fb8500',
       },
     },
   },
