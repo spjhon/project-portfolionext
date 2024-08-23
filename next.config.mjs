dc9917b5375d7a0a6b9e7c4b94c6f19254dc0dcf
spjhon <spjhon@gmail.com>
2024-08-22 21:50:07 -0500

    base theme dark light completo con about incluido

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {//There two lines are added in order to build the static site
+    output: 'export',
+    images: { unoptimized: true },};
 
 export default nextConfig;
+
+//Este es el comando para el server: npx serve@latest out
