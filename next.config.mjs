64180f5af050d0bfc4d0d82e89fb19468c413032
Juan Aristizabal <spjhon@gmail.com>
2024-08-23 11:50:32 -0500

    se deja el  next config como estaba y se modifica el nextjs.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,12 @@
-
+import createNextIntlPlugin from 'next-intl/plugin';
  
-import nextIntl from "next-intl/plugin";
+const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
     images: { unoptimized: true },
 };
-
-// Use nextIntl to extend your configuration with the next-intl plugin
-const withNextIntl = nextIntl("./src/i18n.ts");
  
 export default withNextIntl(nextConfig);
 
