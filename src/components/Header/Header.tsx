49974ccebfb45aca59a85629fd43395622aa9647
Juan Aristizabal <spjhon@gmail.com>
2024-08-26 11:16:43 -0500

    se agrega el cambio de idioma

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,17 @@
 // components/Header.tsx
 
-import {Link} from '../../navigation';
+import NavigationLink from "../NavigationLink"
+import LanguageSwitcher from "../LocaleSwitcher/LocaleSwitcher"
 
 export default function Header() {
     return (
         <header className="bg-gray-200 shadow">
             <nav className="container flex px-2 py-2 gap-5 ">
-                <Link href="/">Home</Link>
-                <Link href="/about">About</Link>
+                <NavigationLink href="/">Home</NavigationLink>
+                <NavigationLink href="/about">About</NavigationLink>
                 
             </nav>
+            <LanguageSwitcher></LanguageSwitcher>
         </header>
     )
 }
