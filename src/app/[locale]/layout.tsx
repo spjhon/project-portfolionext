694efe0563cf882e0f111ec661aa42c76123dc54
Juan Aristizabal <spjhon@gmail.com>
2024-09-06 14:28:45 -0500

    se adicona una nuev ruta a la imagen de opengraph

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -62,7 +62,7 @@ export async function generateMetadata({ params: { locale } }: {
       type: 'website',
       images: [
         {
-          url: '/heroBackgrounds/openGraph.webp',
+          url: '/heroBackgrounds/opengraph-image.jpg',
           alt: 'Juan Portfolio',
         }
       ],
