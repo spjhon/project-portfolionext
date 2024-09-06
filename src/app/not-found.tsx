3c249a90a8cc8925d172b6531858a1bc3bc793c6
Juan Aristizabal <spjhon@gmail.com>
2024-09-06 18:49:25 -0500

    intento 2

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,16 +11,6 @@ import styles from "./notFound.module.css";
 export default function NotFound() {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Page not found / Página no encontrada en este sitio." />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="robots" content="index, follow" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap" />
-        <title>Not Found / No Encontrado</title>
-      </head>
       <body>
         <div className={styles.container}>
           <Image className={styles.picture} src={cavernicola} alt="Cavernicola" />
