d2abe44bf3586c60fbb2898aaad4832283dd69aa
Juan Aristizabal <spjhon@gmail.com>
2024-09-10 14:56:57 -0500

    adelanto a HeroMain y posicoinamiento mobil

--- src/components/Hero/HeroMain.tsx
+++ src/components/Hero/HeroMain.tsx
@@ -1,22 +1,21 @@
-
 import React from "react";
 import NeonTitle from "../neonTitle/NeonTitle";
-
+import { useTranslations } from "next-intl";
 
 const HeroMain: React.FC = () => {
-
+  const t = useTranslations("HeroMain");
 
   return (
     <div className="h-full p-5 lg:p-24 flex flex-col justify-center space-y-2">
-      <p className="text-sm text-barra">Mi nombre es</p>
+      <p className="text-sm text-barra">{t("upperText")}</p>
       <div className="relative ">
         <NeonTitle></NeonTitle>
-        <h1 className="text-4xl font-bold text-barra">JUAN CAMILO</h1>
+        <h1 className="text-2xl lg:text-4xl font-bold text-barra">JUAN ARISTIZABAL</h1>
       </div>
-      <p className="text-sm text-barra">Front-End Developer</p>
+      <p className="text-sm text-barra">{t("subText")}</p>
       <div className="w-16 h-0.5 bg-highlight my-2"></div>
-      <button className="px-4 py-2 bg-secondary text-white font-semibold rounded">
-        Hire Me
+      <button className="px-4 py-2 bg-secondary text-white font-semibold rounded transform transition-transform duration-300 hover:scale-110">
+      {t("button")}
       </button>
     </div>
   );
